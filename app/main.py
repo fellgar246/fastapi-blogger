91d31f9de03e0a1c4948cd7b5ed9dc87d228f424
Luis Felipe Garcia <fellgar246@gmail.com>
2026-03-14 13:40:59 -0600

    add db.py and main.py

--- app/main.py
+++ app/main.py
@@ -7,23 +7,9 @@ from math import ceil
 from sqlalchemy import create_engine, Integer, String, Text, DateTime, select, func, UniqueConstraint, ForeignKey, Table, Column
 from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase, Mapped, mapped_column, relationship, selectinload, joinedload
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
-print(f"Using database: {DATABASE_URL}")
-
-engine_kwargs = {}
-if DATABASE_URL.startswith("sqlite"):
-    engine_kwargs["connect_args"] = {"check_same_thread": False}
-
-engine = create_engine(DATABASE_URL, echo=True, future=True, **engine_kwargs)
-
-SessionLocal = sessionmaker(
-    bind=engine, autoflush=False, autocommit=False, class_=Session)
-
-
-class Base(DeclarativeBase):
-    pass
-
+load_dotenv()
 
 post_tags = Table(
     "post_tags",
@@ -80,14 +66,6 @@ class PostORM(Base):
 
 
 Base.metadata.create_all(bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 app = FastAPI(title="Mini Blog")
