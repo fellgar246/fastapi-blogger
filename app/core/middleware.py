5d6c1529f3462f54265bd68142669256d2541b56
Luis Felipe Garcia <fellgar246@gmail.com>
2026-04-19 13:43:48 -0600

    add middleware for request ID generation and IP blocking

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -1,8 +1,22 @@
 import time
-from fastapi import FastAPI, Request
+from urllib import response
+import uuid
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+BLACKLIST = {}
 
 
 def register_middleware(app: FastAPI):
+
+    app.add.middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     @app.middleware("http")
     async def add_process_time_header(request: Request, call_next):
         start = time.perf_counter()
@@ -17,3 +31,17 @@ def register_middleware(app: FastAPI):
         response = await call_next(request)
         print(f"**SALIDA: {response.status_code}")
         return response
+
+    @app.middleware("http")
+    async def add_request_id_header(request: Request, call_next):
+        request_id = str(uuid.uuid4())
+        response = await call_next(request)
+        response.headers["X-Request-ID"] = request_id
+        return response
+
+    @app.middleware("http")
+    async def block_ip_middleware(request: Request, call_next):
+        client_ip = request.client.host
+        if client_ip in BLACKLIST:
+            raise HTTPException(status_code=403, detail="Forbidden")
+        return await call_next(request)
